8b27f8566f20b27d46a071fb5e97ce6d112ef569
andrei.lisa <andreilisa2001@gmail.com>
2021-10-21 10:31:44 +0300

    modifi

--- demo/src/main/java/com/example/demo/service/JsonService.java
+++ demo/src/main/java/com/example/demo/service/JsonService.java
@@ -1,12 +1,7 @@
-
-
 package com.example.demo.service;
 
 import com.example.demo.mapper.JsonRepository;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 import com.jayway.jsonpath.JsonPath;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
-import java.util.*;
+import java.util.List;
 
 @Component
 public class JsonService {
@@ -22,12 +17,14 @@ public class JsonService {
     private String keys;
     @Value("${reader.keyArr}")
     private String keysArr;
+    @Value("${file.path}")
+    private String path;
 
     @Autowired
     private JsonRepository jsonMapper;
 
     public void write() throws IOException {
-        InputStream inputStream = new FileInputStream("E:\\Folder\\doc1.json");
+        InputStream inputStream = new FileInputStream(path);
         Reader inputStreamReader = new InputStreamReader(inputStream);
         JsonReader jsonReader = new JsonReader(inputStreamReader);
 
@@ -40,32 +37,28 @@ public class JsonService {
         while (jsonReader.hasNext()) {
             Object obj = gson.fromJson(jsonReader, Object.class);
             StringBuilder builder = new StringBuilder();
-
-            builder.append("insert into json3 (").append(keys).append(keysArr).append(")").append("values(");
+            String jsonInString = gson.toJson(obj);
+            builder.append("insert into json2 (").append(keys).append(keysArr).append(")").append("values(");
             for (String key : keys.split(",")) {
-                String value = JsonPath.read(obj, key);
+                String value = JsonPath.read(jsonInString, key);
                 builder.append("'").append(value).append("',");
             }
             for (String key : keysArr.split(";")) {
-                List<String> value = JsonPath.read(obj, key);
-                builder.append("'").append("\"").append(value).append("\"").append("',");
+                List<String> value = JsonPath.read(jsonInString, key);
+                builder.append("'").append(value).append("',");
 
             }
-            for (int i = 0; i < builder.length(); i++) {
-                if (builder.charAt(i) == '=') {
-                    builder.setCharAt(i, ':');
-                }
-            }
-                builder.deleteCharAt(builder.length() - 1);
-                builder.append(")");
+
+            builder.deleteCharAt(builder.length() - 1);
+            builder.append(")");
             jsonMapper.save(builder.toString());
 
-                System.out.println(builder);
+            System.out.println(builder);
 
 
-            }
         }
-
     }
+
+}
 
 
