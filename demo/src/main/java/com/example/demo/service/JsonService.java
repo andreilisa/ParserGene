d0e1c6aebf5434c38dd9c2e0bf59dad90dcb918f
andrei.lisa <andreilisa2001@gmail.com>
2021-10-21 13:39:32 +0300

    modify

--- demo/src/main/java/com/example/demo/service/JsonService.java
+++ demo/src/main/java/com/example/demo/service/JsonService.java
@@ -1,7 +1,9 @@
 package com.example.demo.service;
 
 import com.example.demo.mapper.JsonRepository;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
 import com.jayway.jsonpath.JsonPath;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +17,13 @@ import java.util.List;
 public class JsonService {
     @Value("${reader.keys}")
     private String keys;
+
     @Value("${reader.keyArr}")
-    private String keysArr;
+    private String keysArray;
+
     @Value("${file.path}")
     private String path;
+
 
     @Autowired
     private JsonRepository jsonMapper;
@@ -33,17 +38,20 @@ public class JsonService {
         jsonReader.beginObject();
         jsonReader.nextName();
         jsonReader.beginArray();
-        Gson gson = new Gson();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         while (jsonReader.hasNext()) {
             Object obj = gson.fromJson(jsonReader, Object.class);
-            StringBuilder builder = new StringBuilder();
             String jsonInString = gson.toJson(obj);
-            builder.append("insert into json2 (").append(keys).append(keysArr).append(")").append("values(");
+            StringBuilder builder = new StringBuilder();
+            builder.append("insert into json2 (").append(keys).append(keysArray).append(")").append("values(");
             for (String key : keys.split(",")) {
+
                 String value = JsonPath.read(jsonInString, key);
                 builder.append("'").append(value).append("',");
+
             }
-            for (String key : keysArr.split(";")) {
+            for (String key : keysArray.split(",")) {
                 List<String> value = JsonPath.read(jsonInString, key);
                 builder.append("'").append(value).append("',");
 
@@ -54,11 +62,6 @@ public class JsonService {
             jsonMapper.save(builder.toString());
 
             System.out.println(builder);
-
-
         }
     }
-
 }
-
-
